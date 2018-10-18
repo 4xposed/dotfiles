d951dd7faa3b8e6d5159bf01a601f16f43b9e9ae
4xposed <stokp1@gmail.com>
2018-10-18 22:19:09 +0200

    Cleanup fish functions

--- .config/fish/functions/functions/brew.fish
+++ .config/fish/functions/functions/brew.fish
@@ -9,11 +9,3 @@ end
 function upgrade
   brew upgrade
 end
-
-function be
- bundle exec $argv
-end
-
-function bi
-  bundle install
-end
---
+++ .config/fish/functions/functions/bundle.fish
@@ -0,0 +1,11 @@
+function be
+ bundle exec $argv
+end
+
+function bi
+  bundle install
+end
+
+function bu
+  bundle update $argv
+end
